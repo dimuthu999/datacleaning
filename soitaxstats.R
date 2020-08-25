bb9721a4464b07f56bcba27f813595b6e7c0da05
Dimuthu Ratnadiwakara <dratnadiwakara2@lsu.edu>
2020-08-25 16:13:26 -0500

    d

--- soitaxstats.R
+++
@@ -1,9 +0,0 @@
-# this script cleans SOI Tax Stats - Migration Data from https://www.irs.gov/statistics/soi-tax-stats-migration-data
-# file format different after year 2011
-
-rm(list=ls())
-
-
-
-
-
